ac6e46359245d93507da0e646422de4836a802b4
jrober84 <jrober84@gmail.com>
2021-11-19 10:50:11 -0500

    updated canu

--- projectX/ext_tools/canu.py
+++ projectX/ext_tools/canu.py
@@ -18,8 +18,38 @@ def run_canu_correct(reads,prefix,out_dir,genome_size,min_length=1000,minOverlap
     reads_file = os.path.join(out_dir,"{}.correctedReads.fasta.gz".format(prefix))
     if os.path.isfile(reads_file):
         is_valid = True
+    results = {
+        'corrected_reads':reads_file,
+        'assembly_file':''
+    }
+    return {'data': results, 'stderr': stderr, 'stdout': stdout,'is_vald':is_valid,'cmd':cmd}
 
-    return {'data': reads_file, 'stderr': stderr, 'stdout': stdout,'is_vald':is_valid,'cmd':cmd}
+def run_canu_assembly(reads,prefix,out_dir,genome_size,min_length=1000,minOverlapLength=500,corOutCoverage=1000,n_threads=1):
+    cmd_args = {'maxThreads=':n_threads,
+                '-p ':prefix,
+                'stopOnLowCoverage=':'False',
+                'genomeSize=':genome_size,
+                'minReadLength=':min_length,
+                'minOverlapLength':minOverlapLength,
+                'corOutCoverage':corOutCoverage,
+                '-d ':out_dir,
+                '-nanopore ':reads}
+    cmd = "canu useGrid=False {}".format((" ".join(f'{k}{v}' for k,v in cmd_args.items())))
+    (stdout, stderr) = run_command(cmd)
+    is_valid = True
+    assembly_file = os.path.join(out_dir,"{}.contigs.fasta".format(prefix))
+    if not os.path.isfile(assembly_file):
+        is_valid = False
+    reads_file = os.path.join(out_dir,"{}.fasta".format(prefix))
+    if not os.path.isfile(reads_file):
+        is_valid = False
+
+    results = {
+        'corrected_reads':reads_file,
+        'assembly_file':assembly_file
+    }
+    return {'data': results, 'stderr': stderr, 'stdout': stdout,'is_vald':is_valid,'cmd':cmd}
+
 
 def get_canu_version():
     cmd = 'canu --version'
